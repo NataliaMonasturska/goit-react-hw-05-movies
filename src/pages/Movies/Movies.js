4970e6ebfb9af378b1b40c02070bb648facabea1
Natalia Monasturska <tjomamon@gmail.com>
2022-08-28 19:44:36 +0300

    start

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -5,7 +5,7 @@ const MY_API_KEY = 'ceffe16ccd7d46ce9932d25cc21ec8d8';
 
 
 export const Movies = () => {
-  const [onSubmitValue, setOnSubmitValue] = useState('');
+  const [query, setQuery] = useState('');
   const[searchParams, setSearchParams] =  useSearchParams();
   
 
@@ -15,22 +15,27 @@ export const Movies = () => {
     console.log(searchParams.get("query"));
   }
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${MY_API_KEY}&query=${searchParams.get("query")}`)
+    if(query === ''){
+      return
+    }
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${MY_API_KEY}&query=${query}`)
           .then(response => {
             return response.json();
           })
           .then(data => {
             console.log(data);
+
           });
          
-  }, [searchParams]);
+  }, [query]);
   return (
     <div>
       <form onSubmit={(e) => {
         e.preventDefault();
         // console.log(e.target.elements.text.value);
-        changeFilter(e.target.elements.text.value);
-       
+        changeFilter(e.target.elements.text.value.trim());
+        setQuery(e.target.elements.text.value.trim());
+       e.target.reset();
       }}>
         <input
          type="text"
